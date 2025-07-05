bf8f51e28873cc158ff3b6d47615b8f732aec5c2
TopMyster <topmyster@outlook.com>
2025-07-05 18:49:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+   let malecb = document.getElementById('male')
+            let femalecb = document.getElementById('female')
+            function submition() {
+                let result
+                let ActivityFactor
+                if (document.getElementById('Sedentary').checked) {
+                    ActivityFactor = 1.2
+                }
+
+                if (document.getElementById('Lightly active').checked) {
+                    ActivityFactor = 1.375
+                }
+
+                if (document.getElementById('Moderately active').checked) {
+                    ActivityFactor = 1.55
+                }
+                if (document.getElementById('Very active').checked) {
+                    ActivityFactor = 1.725
+                }
+            
+                if (malecb.checked) {
+                    result =  [66 + (6.23 * document.getElementById('weight').value) + (12.7 * document.getElementById('height').value) - (6.8 * document.getElementById('age').value)] * ActivityFactor - 500
+                }
+                 if (femalecb.checked) {
+                    result =  [655 + (4.35 * document.getElementById('weight').value) + (4.7 * document.getElementById('height').value) - (4.7 * document.getElementById('age').value)] * ActivityFactor - 500
+                }
+
+              document.getElementById('result').textContent = Math.floor(result) 
+            }
